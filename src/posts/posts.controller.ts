de501defccae7f36a9042afe044cf1e940c87c96
KEN <83224463+hyeon17@users.noreply.github.com>
2026-04-07 23:25:17 +0900

    style: Prettier 설정 통일 및 코드 포맷팅 적용
    - semi: false, printWidth: 120, endOfLine: auto 설정 추가
    - 전체 소스 파일 세미콜론 제거 및 포맷팅 통일

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,5 +1,5 @@
-import { Controller } from '@nestjs/common';
-import { PostsService } from './posts.service';
+import { Controller } from '@nestjs/common'
+import { PostsService } from './posts.service'
 
 @Controller('posts')
 export class PostsController {
